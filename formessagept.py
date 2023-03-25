7911960468146cf31cf452146a5a5e64bdd39a75
kostotpe <a19910304@hotmail.com>
2023-03-25 15:14:46 +0800

    v5

--- formessagept.py
+++ formessagept.py
@@ -4,7 +4,7 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0: # %是求餘數的意思 例子是跟1000求餘數 除的意思
+        if count % 1000 == 0: # %是求餘數的意思 例子是跟1000求餘數 就是除
             print(len(data))
 
 print('檔案讀取完了，總共有', len(data), '筆資料')
@@ -28,3 +28,23 @@ for d in data:
         good.append(d) # a in 'abc' -> True , b in 'abc' -> false
 print('共有', len(good), '筆留言提到good')
 print(good[0])
+
+# 搜尋good快寫法
+# output = [(number - 1) for number in referencr if number % 2 == 0]
+# (number - 1) 運算
+# number 變數
+# referencr 清單
+# number % 2 == 0 篩選條件
+good = [d for d in data if 'good' in d]
+print('have', len(good), 'good')
+# 練習取bad
+bad = [d for d in data if 'bad' in d]
+print('have', len(bad), 'bad')
+# 再進階
+# 'bad' in bad是否有在清單內，True or false
+bad = ['bad' in d for d in data]
+print(bad)
+# '再進階'原始寫法
+bad = []
+for d in data:
+    bad.append('bad' in d)
